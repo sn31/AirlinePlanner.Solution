315fc7eb68bc627bcfbf17391d369b8d2d098d25
Meria Thomas and Skye Nguyen <meria+skye@>
2018-09-24 15:32:01 -0700

    add Save method to Flight class

--- AirlinePlanner/Models/Flight.cs
+++ AirlinePlanner/Models/Flight.cs
@@ -7,12 +7,37 @@ namespace AirlinePlanner.Models
 {
     public class Flight
     {
-        public int Id {get;set;}
-        public string Name {get;set;}
+        public int Id { get; set; }
+        public string Name { get; set; }
 
-        public DateTime DepartureTime {get;set;}
+        public DateTime DepartureTime { get; set; }
 
-        public string Status {get;set;}
-        
+        public string Status { get; set; }
+
+        public Flight (string newName, DateTime newDepartureTime, string newStatus, int id = 0)
+        {
+            Name = newName;
+            DepartureTime = newDepartureTime;
+            Status = newStatus;
+            Id = id;
+        }
+
+        public void Save ()
+        {
+            MySqlConnection conn = DB.Connection ();
+            conn.Open ();
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.CommandText = @"INSERT INTO flights WHERE (name, departure_time, status) VALUES (@newName, @newDepartureTime,@newStatus);";
+            cmd.Parameters.AddWithValue("@newName",this.Name);
+            cmd.Parameters.AddWithValue("@newDepartureTime",this.DepartureTime);
+            cmd.Parameters.AddWithValue("@newStatus",this.Status);
+
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
     }
 }
